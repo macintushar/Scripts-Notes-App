170a1e2a5387f59695cdaa62a314da8aacaceaf5
Tushar Selvakumar <tusharkumar91111@gamil.com>
2023-11-15 09:35:53 +0530

    Fixed View Note page

--- web/src/components/view-note.tsx
+++ web/src/components/view-note.tsx
@@ -1,33 +1,78 @@
 import { useState, useEffect } from 'react';
 import {  useNavigate, useParams } from 'react-router-dom';
 import LoadingScreen from './loading';
+import { DocumentData, doc, getDoc, serverTimestamp, updateDoc } from 'firebase/firestore';
+import { app, db } from '../firebaseConfig';
+import { getAuth } from 'firebase/auth';
 
-export default function ViewNote({ session }) {
-    const [userNotes, setUserNotes] = useState<any>(null);
-    const params = useParams();
-    const note_uid = params.id;
+export default function ViewNote() {
+  const [userNotes, setUserNotes] = useState<DocumentData>();
+  
+  const params = useParams();
+  const note_uid = params.id;
+  
+  const auth = getAuth(app);
+  const userId = auth.currentUser?.uid;
 
-    const navigate = useNavigate();
+  let lastModified;
 
-    async function handleClose() {
+  const navigate = useNavigate();
+
+  const handleClose = async () => {
+    try {
+      const noteRef = doc(db, `users/${userId}/notes/${note_uid}`);
+      const noteDoc = await getDoc(noteRef);
+
+      if (noteDoc.exists()) {
         const new_title = (document.getElementById("note-title") as HTMLInputElement).value;
-        const new_body = (document.getElementById("note-body") as HTMLInputElement).value;
-        console.log(note_uid,new_title,new_body)
-        navigate("/")
-    }
-    const fetchData = async () => {
-      const sample = [
-        {
-          note_body:"Text",
-          note_title:"123"
+        const new_content = (document.getElementById("note-content") as HTMLInputElement).value;
+                
+        if (lastModified > noteDoc.data().dateModified) {
+          await updateDoc(noteRef, {
+            title: new_title,
+            content: new_content,
+            lastModified: lastModified,
+          });
+
+          console.log('Note updated successfully');
+        } else {
+          console.log('Note was modified by someone else');
         }
-      ]
-      setUserNotes(sample)
-    };
+      } else {
+        console.log('Note does not exist');
+      }
+
+      navigate('/');
+    } catch (error) {
+      console.error('Error updating note:', error);
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      const noteRef = doc(db, `users/${userId}/notes/${note_uid}`);
+      const noteDoc = await getDoc(noteRef);
+
+      if (noteDoc.exists()) {
+        const noteData = noteDoc.data();
+        setUserNotes(noteData);
+        console.log(noteData)
+      } else {
+        console.log('Note does not exist');
+      }
+    } catch (error) {
+      console.error('Error fetching note:', error);
+    }
+  };
+
+  const handleTextChange = () => {
+    lastModified = serverTimestamp();
+  };
+  
 
   useEffect(() => {
     fetchData();
-  }, [session]);
+  }, []);
 
   if (!userNotes) {
     return <LoadingScreen />;
@@ -37,9 +82,27 @@ export default function ViewNote({ session }) {
     <>
         <div className="flex justify-center">
             <div className="bg-slate-400 dark:bg-slate-700 p-4 h-full md:p-20 rounded md:h-fit container mt-20 w-full">
-                <input className='w-full p-3 focus:ring-0 dark:text-black font-extrabold block rounded-xl' id="note-title" placeholder="Title" defaultValue={userNotes[0].note_title ? userNotes[0].note_title : "" }></input>
-                <textarea className='w-full h-min-6xl p-3 focus:ring-0 focus:border-0 mt-2 dark:text-black block rounded-xl' rows={16} id="note-body" placeholder="Some text here.." defaultValue={userNotes[0].note_body ? userNotes[0].note_body : "Enter text here...." }></textarea>
-                <button className='px-4 py-2 mt-2 button rounded-full dark:bg-slate-500 bg-slate-100 text-black dark:text-slate-50 font-extrabold' onClick={handleClose}>Close</button>
+                <input 
+                  className='w-full p-3 focus:ring-0 dark:text-black font-extrabold block rounded-xl' 
+                  id="note-title" 
+                  placeholder="Title" 
+                  defaultValue={userNotes.title ? userNotes.title : "" }
+                  onChange={handleTextChange}
+                />
+                <textarea 
+                  className='w-full h-min-6xl p-3 focus:ring-0 focus:border-0 mt-2 dark:text-black block rounded-xl' 
+                  rows={16}
+                  id="note-content"
+                  placeholder="Some text here.."
+                  defaultValue={userNotes.content ? userNotes.content : "Enter text here...." }
+                  onChange={handleTextChange}
+                />
+                <button 
+                  className='px-4 py-2 mt-2 button rounded-full dark:bg-slate-500 bg-slate-100 text-black dark:text-slate-50 font-extrabold' 
+                  onClick={handleClose}
+                >
+                  Close
+                </button>
             </div>
         </div>
     </>
